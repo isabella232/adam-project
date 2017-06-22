e7a1879580ea365edb0c4f1d56e4b5bdf8191a6a
Christophe Maldivi <christophe.maldivi@orange.com>
2017-06-22 11:45:01 +0200

    feat(glycaemia screen): first version with date/hour/value

---
+++ app/src/main/java/org/project/adam/glycaemia/GlycaemiaActivity.java
@@ -0,0 +1,13 @@
+package org.project.adam.glycaemia;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+import org.project.adam.R;
+
+@SuppressLint("Registered")
+@EActivity(R.layout.input_glycaemia)
+public class GlycaemiaActivity extends AppCompatActivity {
+
+}
