8358a101034ae123e3e704307fa36bf7713106f7
Ben Einaudi <benjamin.einaudi@orange.com>
2017-06-22 16:00:39 +0200

    Add "add diet" button

--- app/src/main/java/org/project/adam/ui/diet/DietFragment.java
+++ app/src/main/java/org/project/adam/ui/diet/DietFragment.java
@@ -2,12 +2,21 @@ package org.project.adam.ui.diet;
 
 import android.annotation.SuppressLint;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.project.adam.BaseFragment;
 import org.project.adam.R;
 
+import timber.log.Timber;
+
 @SuppressLint("Registered")
 @EFragment(R.layout.fragment_diet)
 public class DietFragment extends BaseFragment {
+
+
+    @Click(R.id.add_diet)
+    public void onAddDietClick(){
+        Timber.d("add diet clicked");
+    }
 
 }
