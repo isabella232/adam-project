ab46a11cc53ba67411047f79789535614c783d6e
Christophe Maldivi <christophe.maldivi@orange.com>
2017-06-22 17:31:01 +0200

    feat(glycaemia screen): WIP insert in DB

--- app/src/main/java/org/project/adam/glycaemia/GlycaemiaActivity.java
+++ app/src/main/java/org/project/adam/glycaemia/GlycaemiaActivity.java
@@ -1,8 +1,10 @@
 package org.project.adam.glycaemia;
 
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +16,17 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.SeekBarProgressChange;
 import org.androidannotations.annotations.ViewById;
+import org.project.adam.AppDatabase;
 import org.project.adam.R;
+import org.project.adam.persistence.Glycaemia;
+import org.project.adam.persistence.GlycaemiaDao;
+import org.project.adam.util.DatabasePopulator;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+
+import lombok.RequiredArgsConstructor;
 
 @SuppressLint("Registered")
 @EActivity(R.layout.input_glycaemia)
@@ -26,6 +35,13 @@ public class GlycaemiaActivity extends AppCompatActivity {
     private static final int DEFAULT_GLYCAEMIA = 70;
     private static final int MIN_GLYCAEMIA = 20;
     private static final int MAX_GLYCAEMIA = 150;
+
+    @RequiredArgsConstructor
+    private class Hour {
+        final int hourOfDay;
+        final int minute;
+    }
+
 
     @ViewById(R.id.glycaemia_date)
     TextView glycaemiaDate;
@@ -42,11 +58,11 @@ public class GlycaemiaActivity extends AppCompatActivity {
     // FIXME: should work with android annotations
     SeekBar seekBarGlycaemia;
 
-    private Date currentDate = new Date();
+    Hour hour;
 
     @AfterViews
     void fillDateAndHour() {
-        Date date = currentDate;
+        Date date = new Date();
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, d MMM");
         glycaemiaDate.setText(simpleDateFormat.format(date));
@@ -74,11 +90,43 @@ public class GlycaemiaActivity extends AppCompatActivity {
     }
 
     void updateTime(int hourOfDay, int minute) {
+        hour = new Hour(hourOfDay, minute);
         glycaemiaHour.setText(hourOfDay + ":" + minute);
     }
 
     @Click(R.id.glycaemia_validate)
     void validate() {
+        new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... voids) {
+                GlycaemiaDao glycaemiaDao = AppDatabase.getDatabase(getApplicationContext()).glycemiaDao();
+                glycaemiaDao.insert(buildGlycemia());
+                return null;
+            }
+        }.execute();
+    }
 
+    private Glycaemia buildGlycemia() {
+        float value = Float.parseFloat(glycaemiaValueMgDl.getText().toString());
+        Date date = new Date();
+        if (hour != null) {
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.HOUR_OF_DAY, hour.hourOfDay);
+            cal.set(Calendar.MINUTE, hour.minute);
+            cal.set(Calendar.SECOND, 0);
+            date = cal.getTime();
+        }
+
+        Glycaemia glycaemia = Glycaemia.builder()
+            .lunchId(1)
+            .date(date)
+            .value(value)
+            .comment("Not implemented yet")
+            .context("Not implemented yet")
+            .build();
+
+        Log.d(getClass().getSimpleName(), glycaemia.toString());
+
+        return glycaemia;
     }
 }
