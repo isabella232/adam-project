0ce9f8e79fb9ee77a4382288bacd15c8ddc32ee2
Arnaud Ruffin <arnaud.ruffin@orange.com>
2017-07-17 09:05:49 +0200

    Launch dev menu on device flip

--- app/src/main/java/org/project/adam/BaseActivity.java
+++ app/src/main/java/org/project/adam/BaseActivity.java
@@ -3,6 +3,10 @@ package org.project.adam;
 import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
@@ -10,6 +14,7 @@ import android.view.KeyEvent;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.KeyDown;
+import org.androidannotations.annotations.SystemService;
 import org.project.adam.alert.AlertScheduler;
 
 import timber.log.Timber;
@@ -22,6 +27,11 @@ public abstract class BaseActivity extends AppCompatActivity implements Lifecycl
 
     private final LifecycleRegistry registry = new LifecycleRegistry(this);
 
+    @SystemService
+    protected SensorManager sensorManager;
+
+    protected Sensor accelerometer;
+
     @Override
     public LifecycleRegistry getLifecycle() {
         return registry;
@@ -30,12 +40,12 @@ public abstract class BaseActivity extends AppCompatActivity implements Lifecycl
     @Bean
     protected AlertScheduler alertScheduler;
 
-    @KeyDown({ KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP })
+    @KeyDown({KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP})
     protected boolean volumeLongPress(KeyEvent keyEvent) {
         if (BuildConfig.DEBUG && keyEvent.isLongPress()) {
-                Timber.d("Did someone called for the secret menu?");
-                showDevMenu();
-                return true;
+            Timber.d("Did someone called for the secret menu?");
+            showDevMenu();
+            return true;
         }
         return false;
     }
@@ -48,7 +58,7 @@ public abstract class BaseActivity extends AppCompatActivity implements Lifecycl
                     public void onClick(DialogInterface dialog, int which) {
                         switch (which) {
                             case 0:
-                                Timber.d("Deleting data");;
+                                Timber.d("Deleting data");
                                 AppDatabase.getDatabase(BaseActivity.this).delete(BaseActivity.this);
                                 break;
                             case 1:
@@ -70,5 +80,50 @@ public abstract class BaseActivity extends AppCompatActivity implements Lifecycl
                 });
             builder.show();
         }
+    }
+
+    private SensorEventListener accelerometerListener = new SensorEventListener() {
+
+        boolean hasBeenDown = false;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float z = event.values[2];
+            if (z > 9 && z < 10){
+                if(hasBeenDown){
+                    showDevMenu();
+                    hasBeenDown = false;
+                }
+            }else if (z > -10 && z < -9){
+                hasBeenDown = true;
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor arg0, int arg1) {
+
+        }
+
+    };
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (BuildConfig.DEBUG) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+            if (accelerometer != null) {
+                sensorManager.registerListener(accelerometerListener, accelerometer, SensorManager.SENSOR_DELAY_UI);
+            }
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (BuildConfig.DEBUG) {
+            sensorManager.unregisterListener(accelerometerListener);
+        }
+        super.onPause();
     }
 }
